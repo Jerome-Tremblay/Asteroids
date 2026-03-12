94246bb21e2af3360a37cc862d3d630b719bae60
Jerome-Tremblay <jerome.tremblaysy@gmail.com>
2026-03-12 09:17:39 +0900

    B: FPS

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     )
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -19,6 +21,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        Clock.tick(60)
+        dt = Clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
